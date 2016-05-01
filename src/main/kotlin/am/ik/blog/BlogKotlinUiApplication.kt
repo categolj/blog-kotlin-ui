61a4a1233e48612e4433edbee7c9b92e7b57a953
making <makingx@gmail.com>
2016-05-01 13:23:52 +0900

    Add Graphite

--- src/main/kotlin/am/ik/blog/BlogKotlinUiApplication.kt
+++ src/main/kotlin/am/ik/blog/BlogKotlinUiApplication.kt
@@ -6,6 +6,9 @@ import com.codahale.metrics.graphite.Graphite
 import com.codahale.metrics.graphite.GraphiteReporter
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.SpringApplication
+import org.springframework.boot.actuate.autoconfigure.ExportMetricWriter
+import org.springframework.boot.actuate.metrics.export.MetricExportProperties
+import org.springframework.boot.actuate.metrics.repository.redis.RedisMetricRepository
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
@@ -13,6 +16,7 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
+import org.springframework.data.redis.connection.RedisConnectionFactory
 import org.springframework.web.client.RestTemplate
 import java.net.URL
 import java.util.concurrent.TimeUnit
@@ -26,6 +30,7 @@ open class BlogKotlinUiApplication {
 
 
     @Bean
+    @Profile("!no-graphite")
     open fun graphiteReporter(@Value("\${HOSTEDGRAPHITE_APIKEY:\${vcap.services.hostedgraphite.credentials.apikey:}}") prefix: String?,
                               @Value("\${HOSTEDGRAPHITE_URL:\${vcap.services.hostedgraphite.credentials.url:http://localhost}}") url: URL,
                               @Value("\${HOSTEDGRAPHITE_PORT:\${vcap.services.hostedgraphite.credentials.port:0}}") port: Int,
@@ -34,6 +39,13 @@ open class BlogKotlinUiApplication {
         val reporter = GraphiteReporter.forRegistry(registry).prefixedWith(prefix).build(Graphite(url.host, port))
         reporter.start(1, TimeUnit.SECONDS)
         return reporter
+    }
+
+    @Bean
+    @Profile("cloud")
+    @ExportMetricWriter
+    open fun redisMetricWriter(export: MetricExportProperties, connectionFactory: RedisConnectionFactory): RedisMetricRepository {
+        return RedisMetricRepository(connectionFactory, export.redis.prefix, export.redis.key);
     }
 }
 
