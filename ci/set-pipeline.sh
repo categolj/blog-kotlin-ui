a662d694ec11372feb44738f4804a40eddde6817
making <makingx@gmail.com>
2016-08-27 14:06:17 +0900

    Change fly target

--- ci/set-pipeline.sh
+++ ci/set-pipeline.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-echo y | fly -t apj sp -p blog-blog-kotlin-ui -c pipeline.yml -l ../../credentials.yml
+echo y | fly -t home sp -p blog-blog-kotlin-ui -c pipeline.yml -l ../../credentials.yml
