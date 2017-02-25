4cebe399380faa3c7bb99295ff02bdd7bcb9a73d
making <makingx@gmail.com>
2017-02-25 22:36:05 +0900

    Update pipeline

--- ci/tasks/selenium.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-export GEOMETRY="$SCREEN_WIDTH""x""$SCREEN_HEIGHT""x""$SCREEN_DEPTH"
-xvfb-run --server-args="-screen 0 $GEOMETRY -ac +extension RANDR" java -jar /opt/selenium/selenium-server-standalone.jar &
-sudo chmown -R seluser ../
-repo/ci/tasks/unit.sh
