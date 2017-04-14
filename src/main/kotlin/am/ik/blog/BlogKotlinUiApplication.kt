7d6edcb204774ec0197cf126f72ed36c41afdfe7
making <makingx@gmail.com>
2017-04-14 16:46:10 +0900

    Use plain RestTemplate

--- src/main/kotlin/am/ik/blog/BlogKotlinUiApplication.kt
+++ src/main/kotlin/am/ik/blog/BlogKotlinUiApplication.kt
@@ -3,15 +3,12 @@ package am.ik.blog
 import am.ik.marked4j.MarkedBuilder
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.client.loadbalancer.LoadBalanced
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
-import org.springframework.http.HttpHeaders
-import org.springframework.http.client.ClientHttpRequestInterceptor
 import org.springframework.web.client.RestTemplate
 
 @SpringBootApplication
@@ -27,12 +24,7 @@ class BlogKotlinUiApplication {
 class RestTemplateConfig {
     @LoadBalanced
     @Bean
-    fun restTemplate(builder: RestTemplateBuilder): RestTemplate {
-        return builder.interceptors(arrayListOf(ClientHttpRequestInterceptor { request, body, execution ->
-            request.headers.add(HttpHeaders.USER_AGENT, "Mozilla/5.0")
-            execution.execute(request, body)
-        })).build()
-    }
+    fun restTemplate() = RestTemplate()
 }
 
 @Configuration
