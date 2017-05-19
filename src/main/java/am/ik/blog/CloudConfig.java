68f4ddb330f7002d041dcca8ce7444644bb29975
making <makingx@gmail.com>
2017-05-20 06:10:32 +0900

    Add CloudConfig

---
+++ src/main/java/am/ik/blog/CloudConfig.java
@@ -0,0 +1,16 @@
+package am.ik.blog;
+
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.cloud.config.java.AbstractCloudConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("cloud")
+public class CloudConfig extends AbstractCloudConfig {
+	@Bean
+	ConnectionFactory rabbitConnectionFactory() {
+		return connectionFactory().rabbitConnectionFactory();
+	}
+}
