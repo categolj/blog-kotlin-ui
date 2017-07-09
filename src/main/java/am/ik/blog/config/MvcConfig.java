34002daf4ac2e0eb133e3b0c27c32c2b3f1fac82
making <makingx@gmail.com>
2017-07-09 13:52:22 +0900

    Add UserAgentMetricsInterceptor

--- src/main/java/am/ik/blog/config/MvcConfig.java
+++ src/main/java/am/ik/blog/config/MvcConfig.java
@@ -5,17 +5,22 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import am.ik.blog.maintenance.MaintenanceInterceptor;
+import am.ik.blog.metrics.UserAgentMetricsInterceptor;
 
 @Configuration
 public class MvcConfig extends WebMvcConfigurerAdapter {
 	private final MaintenanceInterceptor maintenanceInterceptor;
+	private final UserAgentMetricsInterceptor userAgentMetricsInterceptor;
 
-	public MvcConfig(MaintenanceInterceptor maintenanceInterceptor) {
+	public MvcConfig(MaintenanceInterceptor maintenanceInterceptor,
+			UserAgentMetricsInterceptor userAgentMetricsInterceptor) {
 		this.maintenanceInterceptor = maintenanceInterceptor;
+		this.userAgentMetricsInterceptor = userAgentMetricsInterceptor;
 	}
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(maintenanceInterceptor);
+		registry.addInterceptor(userAgentMetricsInterceptor);
 	}
 }
