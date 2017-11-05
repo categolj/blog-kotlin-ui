557380bb7b0229e0819a6065f446beb50a315f29
making <makingx@gmail.com>
2017-11-05 16:03:16 +0900

    Update pipeline

--- ci/set-pipeline.sh
+++ ci/set-pipeline.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-echo y | fly -t home sp -p blog-blog-kotlin-ui -c pipeline.yml -l ../../credentials.yml
+fly -t home sp -p blog-blog-kotlin-ui \
+    -c `dirname $0`/pipeline.yml \
+    -l `dirname $0`/../../credentials.yml
